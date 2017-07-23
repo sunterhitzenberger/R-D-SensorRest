37df963e5cae905933e8efa0a3de7362b7a6791a
SebUhb <sunterhitzenberger.itsb-b2013@fh-salzburg.ac.at>
2017-07-23 08:21:04 +0200

    add a start DB and GUI Skript

---
+++ StartGuiAndDB.bat
@@ -0,0 +1,6 @@
+cd ../R-D-Frontend\code
+call npm run build:dll
+call npm run start:dev-server
+cd ../../\R-D-SensorRest
+call java -cp h2-latest.jar org.h2.tools.Server -baseDir ./ -webAllowOthers -tcpAllowOthers
+pause
